cf32bd44891ae48020ff6c84874f4b96dce27cba
parnya azimi <you@example.com>
2025-02-10 16:58:51 +0330

    add hw 1/6

---
+++ S01/HW/Exercise 2.5/CToF/Program.cs
@@ -0,0 +1,21 @@
+﻿//Part2
+namespace CToF;
+
+class Program
+{
+    public static void Convert_temperature(double C)
+    {
+        double F=(C*9/5)+32;
+        double C_round =Math.Round(C, 2);
+        double F_round =Math.Round(F, 2);
+        string output=Convert.ToString(C_round)+" : "+Convert.ToString(F_round);
+        Console.WriteLine(output);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your number(degrees in Celsius ): ");
+        var input=Console.ReadLine();
+        double input1=Convert.ToDouble(input);
+        Convert_temperature(input1);
+    }
+}
---
+++ S01/HW/Exercise 2.5/Quadradic/Program.cs
@@ -0,0 +1,44 @@
+﻿//Part6
+namespace Quadradic;
+
+class Program
+{
+    public static double[] Quadradic(double A,double B,double C)
+    {
+        double delta=(B*B)-(4*A*C);
+        if (delta<0)
+            return new double[] { };
+            
+        else if (delta==0)
+        {
+            double X1=-B/(2*A);
+            return new double[] {X1,X1};
+        }
+        else
+        {
+            double X1=((-B)+ Math.Sqrt(delta) )/(2*A);
+            double X2=((-B) - Math.Sqrt(delta))/(2*A);
+            return new double[] {X1,X2};
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your number1: ");
+        var inputA=Console.ReadLine();
+
+        Console.WriteLine("Please enter your number2: ");
+        var inputB=Console.ReadLine();
+
+        Console.WriteLine("Please enter your number3: ");
+        var inputC=Console.ReadLine();
+
+        double[] result=Quadradic(Convert.ToDouble(inputA),Convert.ToDouble(inputB),Convert.ToDouble(inputC));
+        if(result.Length==0)
+            Console.WriteLine("The equation has not real root");
+        else
+        {
+            Console.WriteLine($"root one: {Convert.ToString(result[0])}");
+            Console.WriteLine($"root two: {Convert.ToString(result[1])}");
+        }
+    }
+}
