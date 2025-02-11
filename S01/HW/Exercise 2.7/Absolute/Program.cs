9b6d66c3159ff62793c189d7d12073c66ff886e8
parnya azimi <you@example.com>
2025-02-11 16:29:18 +0330

    add Exercise 2.7

---
+++ S01/HW/Exercise 2.7/Absolute/Program.cs
@@ -0,0 +1,21 @@
+﻿//Part1
+namespace Absolute;
+using System;
+
+class Program
+{
+    public static double Absolute_value(double x)
+    {
+        if(x>=0)
+            return x;
+        return -x;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine (Absolute_value(-100));
+        Console.WriteLine (Absolute_value(0));
+        Console.WriteLine (Absolute_value(-1));
+        Console.WriteLine (Absolute_value(1));
+        Console.WriteLine (Absolute_value(1000));
+    }
+}
