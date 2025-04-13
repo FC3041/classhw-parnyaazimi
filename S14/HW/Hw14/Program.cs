a3fdcd0a4ef527b3f04996d1f17e81d751e46a98
parnya azimi <you@example.com>
2025-04-13 19:24:36 +0330

    Add S14.HW

---
+++ S14/HW/Hw14/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hw14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PolarVector P1=new PolarVector(5,57);
+        PolarVector P2=new PolarVector(2,104);
+
+        MyVector<double> P3=P1.CartesianVector();
+        Console.WriteLine($"{P3.X},{P3.Y}");
+    }
+}
