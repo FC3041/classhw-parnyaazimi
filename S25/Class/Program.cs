398632de8b567571978fc8f4384677127a7f945d
parnya azimi <you@example.com>
2025-05-22 17:58:46 +0330

    Add S25.HW

---
+++ S25/Class/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace S25;
+
+class Program343243
+{
+    static double dowork(object obj)
+    {
+        double n = (double)obj;
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} => {n}");
+        double d = 1;
+        for (int i = 1; i < n; i++)
+            d = (d * i) % (Math.Pow(10,3)+7);
+        return d;
+    }
+
+    static async Task Main(string[] args)
+    {
+        double lastValue = 1000;
+        for (int i = 0; i < 20; i++)
+        {
+            lastValue = await Task<double>.Factory
+                        .StartNew(dowork, lastValue);            
+        }
+    }
+
+
+    static void Main4(string[] args)
+    {
+        double sum = 0;
+        List<Task> tasks = new List<Task>();
+        for (int i = 1; i < 20; i++)
+        {
+            tasks.Add(Task<double>.Factory
+                .StartNew(dowork, i * 1000)
+                .ContinueWith(t => //Interlocked.Add(ref )
+                {
+                    lock (tasks)
+                    {
+                        sum += t.Result;
+                    }
+                }
+                ));
+        }
+        Task.WaitAll(tasks.ToArray());
+        System.Console.WriteLine(sum);
+    }
+
+    static void Main3(string[] args)
+    {
+        var tasks = new Task<double>[19];
+        double sum = 0;
+        for (int i = 1; i < 20; i++)
+        {
+            tasks[i - 1] = Task<double>.Factory.StartNew(dowork, i * 10_000_000);
+        }
+        Task.WaitAll(tasks);
+
+        foreach (var t in tasks)
+            sum += t.Result;
+        System.Console.WriteLine(sum);
+    }
+
+    static void Main2(string[] args)
+    {
+        var t = Task.Run(() => dowork(10_000_000));
+        while (true)
+        {
+            if (t.Status == TaskStatus.RanToCompletion)
+                break;
+            System.Console.WriteLine("Waiting");
+            Task td = Task.Delay(10);
+            td.Wait();
+        }
+        System.Console.WriteLine(t.Result);
+    }
+}
---
+++ S25/HW/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace HW;
+
+class Program
+{
+    static async Task Download(string link)
+    {
+        using var client = new HttpClient();
+        string result = await client.GetStringAsync(link);
+        string pattern =  @"(?:src|href)\s*=\s*[""'](?<url>[^""']+)[""']";
+        var tasks = new List<Task>();
+        Stopwatch sw = Stopwatch.StartNew();
+        object obj = new object();
+        foreach (Match match in Regex.Matches(result, pattern, RegexOptions.IgnoreCase))
+        {
+            tasks.Add(Task.Factory.StartNew(async () =>
+                    {
+                        try
+                        {
+                            string url = match.Groups["url"].Value;
+                            string filename = Path.GetFileName(url);
+                            var bytes = client.GetByteArrayAsync(url).Result;
+                            await File.WriteAllBytesAsync(filename, bytes);
+                            Console.WriteLine(filename + " => " + url);
+                            await Download(url);
+                        }
+                        catch (System.AggregateException ae)
+                        {
+                            System.Console.WriteLine(ae.Message);
+                        }
+                    }
+                    ));
+        }
+        await Task.WhenAll(tasks);
+        System.Console.WriteLine($"======{sw.Elapsed.ToString()}=======");
+        //======00:00:02.3574624=======
+    }
+
+    static async Task DownloadWithTask(string link)
+    {
+        using var client = new HttpClient();
+        string result = await client.GetStringAsync(link);
+        string pattern = @"(?:src)\s*=\s*[""'](?<url>https?://[^""']+)[""']";
+        var tasks = new List<Task>();
+        Stopwatch sw = Stopwatch.StartNew();
+        object obj = new object();
+        foreach (Match match in Regex.Matches(result, pattern, RegexOptions.IgnoreCase))
+        {
+            tasks.Add(Task.Factory.StartNew(async () =>
+                    {
+                        try
+                        {
+                            string url = match.Groups["url"].Value;
+                            string filename = Path.GetFileName(url);
+                            var bytes = client.GetByteArrayAsync(url).Result;
+                            await File.WriteAllBytesAsync(filename, bytes);
+                            Console.WriteLine(filename + " => " + url);
+                        }
+                        catch (System.AggregateException ae)
+                        {
+                            System.Console.WriteLine(ae.Message);
+                        }
+                    }
+                    ));
+        }
+        await Task.WhenAll(tasks);
+        System.Console.WriteLine($"======{sw.Elapsed.ToString()}=======");
+        //======00:00:02.3574624=======
+    }
+
+
+    static async Task DownloadWithoutTask(string link)
+    {
+        using var client = new HttpClient();
+        string result = await client.GetStringAsync(link);
+        string pattern = @"(?:src)\s*=\s*[""'](?<url>https?://[^""']+)[""']";
+        Stopwatch sw = Stopwatch.StartNew();
+        foreach (Match match in Regex.Matches(result, pattern, RegexOptions.IgnoreCase))
+        {
+            try
+            {
+                string url = match.Groups["url"].Value;
+                var bytes = client.GetByteArrayAsync(url).Result;
+                string filename = Path.GetFileName(url);
+                File.WriteAllBytes(filename, bytes);
+                Console.WriteLine(filename + " => " + url);
+            }
+            catch (System.AggregateException ae)
+            {
+                System.Console.WriteLine(ae.Message);
+            }
+        }
+        System.Console.WriteLine($"======{sw.Elapsed.ToString()}=======");
+        //======00:00:11.5880793=======
+    }
+
+    static async Task Main(string[] arg)
+    {
+        await Download("https://www.tabnak.ir/");
+    }
+}
